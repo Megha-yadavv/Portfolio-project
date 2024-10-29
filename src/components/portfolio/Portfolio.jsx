d2d5bb6498d5634cae1afd8d81de9d47870da2b2
megha yadav <meghayadav5216@gmail.com>
2024-10-29 21:47:47 +0530

    added content

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -11,24 +11,18 @@ export default function Portfolio() {
         <motion.h1>Projects</motion.h1>
         <motion.div className="work-container">
           <motion.div className="work project1">
-            
-              <motion.div className="see-more" style={{color:"black"}}>
-                <motion.h2>See More</motion.h2>
-              </motion.div>
-           
-
             <motion.div className="layer">
-              <h3>Restraurant Menu</h3>
+              <h3>Food Delivery App</h3>
               <p>
-                <span>Tools used: Html,CSS, JavaScript</span>
-                <br />
-                menu landing page was designed using HTML and CSS, ensuring a
-                seamless blend of structure and style. Every detail, from layout
-                to color scheme, was carefully crafted to create a visually
-                appealing, responsive and user-friendly experience.
+                <span>Tools used: ReactJs</span>
+                <br />A responsive food delivery app built with React.js,
+                allowing users to browse food categories, view item details, and
+                manage a cart. This project demonstrates dynamic filtering,
+                state management, and adaptive design for a seamless user
+                experience.
               </p>
               <motion.a
-                href="https://megha-yadavv.github.io/menu-landingpage/"
+                href="https://megha-yadavv.github.io/food-delivery-app/"
                 whileHover={{ scale: 1.2 }}
                 whileTap={{ scale: 0.8, delay: 0.5 }}
               >
@@ -37,11 +31,6 @@ export default function Portfolio() {
             </motion.div>
           </motion.div>
           <motion.div className="work project2">
-          
-              <motion.div className="see-more" style={{color: "black"}}>
-                <motion.h2>See More</motion.h2>
-              </motion.div>
-           
             <motion.div className="layer">
               <h3>To-Do-List</h3>
               <p>
@@ -60,11 +49,6 @@ export default function Portfolio() {
             </motion.div>
           </motion.div>
           <motion.div className="work project3">
-         
-              <motion.div className="see-more" style={{}}>
-                <motion.h2>See More</motion.h2>
-              </motion.div>
-           
             <motion.div className="layer">
               <h3>Tic-Tac-Toe</h3>
               <p>
